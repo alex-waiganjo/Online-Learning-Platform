434fe93b18d5f7d1a79fd1c2768e6032ce4e6a30
alex-waiganjo <alexwaiganjo934@gmail.com>
2024-10-17 13:31:26 +0300

    Online Learning Platform

---
+++ Readme.md
@@ -0,0 +1,46 @@
+# Online Learning Platform
+
+## Users
+ - Student
+ - Instructor
+ - Approver
+
+### Db Models
+ 
+ ### Student
+ - Student id
+ - Student firstname
+ - Student lastname
+ - Student email
+ - Student Password
+
+ ### Instructor
+ - Instructor id
+ - Instructor firstname
+ - Instrucutor lastname
+ - Instrucutor email
+ - Instrucutor Password
+ 
+
+ ### Course
+ - Course id
+ - Course name
+ - Course description
+ - Course Image
+ - Course author
+
+
+ ### Approver
+ - Approver id
+ - Approver firstname
+ - Approver lastname
+ - Approver email
+ - Approver password
+
+### Technologies Used
+ - Python(Flask) , flask-migrate, flask-jwt
+ - Html,Css,Bootstap and Js
+ - DB: Sqlite
+
+
+
---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
+app = Flask(__name__)
+
+# Configs before initialization
+app.config['SECRET_KEY'] = 'Onlinesysmage'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///Online_Learning_Platform.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# Initialize database, JWT, and migration
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+migrate = Migrate(app, db)  # Bind app and db to Migrate
+
+# Models
+
+class Students(db.Model):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    st_fname = db.Column(db.String(80), unique=True, nullable=False)
+    st_lname = db.Column(db.String(80), unique=True, nullable=False)
+    st_email = db.Column(db.String(80), unique=True, nullable=False)
+    st_password = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'{self.st_fname}'
+
+
+class Instructors(db.Model):
+    __tablename__ = 'instructors' 
+
+    id = db.Column(db.Integer, primary_key=True)
+    ins_fname = db.Column(db.String(80), unique=True, nullable=False)
+    ins_lname = db.Column(db.String(80), unique=True, nullable=False)
+    ins_email = db.Column(db.String(80), unique=True, nullable=False)
+    ins_password = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'{self.ins_fname}'
+
+
+class Courses(db.Model):
+    __tablename__ = 'courses'  # Fixed capitalization to match convention
+
+    id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String(80), unique=True, nullable=False)
+    course_description = db.Column(db.String(80), unique=True, nullable=False)
+    course_image = db.Column(db.String(80), unique=True, nullable=False)
+    course_author = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'{self.course_name}'
+
+# Routes
+@app.route("/", methods=['GET'])
+def index_page():
+    return render_template("home.html")
+
+
+@app.route("/student_register", methods=['GET', 'POST'])
+def student_register():
+    if request.method == 'POST':
+       fname= request.form.get('fname')
+       lname= request.form.get('lname')
+       email= request.form.get('email')
+       pwd= request.form.get('password')
+
+       new_student = Students(st_fname=fname,st_lname=lname,st_email=email,st_password=pwd)
+       db.session.add(new_student)
+       db.session.commit()
+
+    return render_template("student_register.html")
+
+
+@app.route("/student_login", methods=['GET', 'POST'])
+def student_login():
+    return render_template("student_login.html")
+
+
+@app.route("/student_home_page", methods=['POST', 'GET'])
+def student_home_page():
+    return render_template("student_home_page.html")
+
+
+@app.route("/instructor_register")
+def instructor_register():
+     if request.method == 'POST':
+       ins_fname= request.form.get('fname')
+       ins_lname= request.form.get('lname')
+       ins_email= request.form.get('email')
+       ins_pwd= request.form.get('password')
+
+       new_student = Instructors(ins_fname=ins_fname,ins_lname=ins_lname,ins_email=ins_email,ins_password=ins_pwd)
+       db.session.add(new_student)
+       db.session.commit()
+     return render_template("instructor_register.html")
+
+
+@app.route("/instructor_login")
+def instructor_login():
+    return render_template("instructor_login.html")
+
+
+@app.route("/instructor_home_page", methods=['POST', 'GET'])
+def instructor_home_page():
+    return render_template("instructor_home_page.html")
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
